a19966adfafb2421546a560601e14d38b680604e
Obi Griffith <obigriffith@gmail.com>
2015-06-09 13:42:27 -0400

    updated paths

--- scripts/Tutorial_Module4_ERCC_expression.pl
+++ scripts/Tutorial_Module4_ERCC_expression.pl
@@ -5,10 +5,10 @@ use warnings;
 
 use IO::File;
 
-my $data_dir = $ENV{RNA_HOME} .'/refs/ERCC';
+my $data_dir = $ENV{RNA_HOME} .'/expression/tophat_counts';
 my $ercc_file = $data_dir .'/ERCC_Controls_Analysis.txt';
-my $counts_file = $ENV{RNA_HOME} .'/expression/tophat_counts/gene_read_counts_table_all.tsv';
-my $ercc_counts_file = $ENV{RNA_HOME} .'/expression/tophat_counts/ercc_read_counts.tsv';
+my $counts_file = $data_dir .'/gene_read_counts_table_all.tsv';
+my $ercc_counts_file = $data_dir .'/ercc_read_counts.tsv';
 
 my $ercc_fh = IO::File->new($ercc_file,'r');
 unless ($ercc_fh) { die('Failed to find file: '. $ercc_file) }
