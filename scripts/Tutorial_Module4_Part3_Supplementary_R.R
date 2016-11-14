cdc681c02cb67178dab6c27165f621acc1d0f000
Jason Walker <jwalker@genome.wustl.edu>
2016-11-14 16:02:47 -0600

    more changes.

--- scripts/Tutorial_Module4_Part3_Supplementary_R.R
+++ scripts/Tutorial_Module4_Part3_Supplementary_R.R
@@ -326,8 +326,8 @@ heatmap.2(data, hclustfun=myclust, distfun=mydist, na.rm = TRUE, scale="none", d
 dev.off()
 
 #The output file can be viewed in your browser at the following url:
-#Note, you must replace cbw## with your own amazon instance number (e.g., "cbw01"))
-#http://__YOUR_IP_ADDRESS__/workspace/rnaseq/de/tophat_cufflinks/ref_only/Tutorial_Part3_Supplementary_R_output.pdf
+#Note, you must replace __YOUR_IP_ADDRESS__ with your own amazon instance IP
+#http://__YOUR_IP_ADDRESS__/workspace/rnaseq/de/ballgown/ref_only/Tutorial_Part3_Supplementary_R_output.pdf
 #To exit R type:
 quit(save="no")
 
