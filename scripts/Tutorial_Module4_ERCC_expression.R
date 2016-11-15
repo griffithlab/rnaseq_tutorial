2b078a64e1f26db07a07c2847a8badef1e49da19
Malachi Griffith <malachig@gmail.com>
2016-11-15 15:53:07 -0500

    Minor updates

--- scripts/Tutorial_Module4_ERCC_expression.R
+++ scripts/Tutorial_Module4_ERCC_expression.R
@@ -15,7 +15,7 @@ count_r_squared = summary(count_model)[['r.squared']]
 
 pdf('Tutorial_Module4_ERCC_expression.pdf')
 ggplot(data, aes(x=logConc, y=logCount)
-       ) + geom_point(aes(shape=Label)
+       ) + geom_point(aes(shape=Label,color=Label)
        ) + geom_smooth(method=lm
        ) + annotate('text', 5, -3,
                     label=paste("R^2 =", count_r_squared, sep=' '))
