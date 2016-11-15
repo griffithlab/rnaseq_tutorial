c7e7bab38475e28fea4b81d5ef8bc575bfca6849
Malachi Griffith <malachig@gmail.com>
2016-11-15 14:24:17 -0500

    add a few more comments to the ballgown exercise

--- scripts/Tutorial_Module4_Part1_ballgown.R
+++ scripts/Tutorial_Module4_Part1_ballgown.R
@@ -3,17 +3,20 @@ library(genefilter)
 library(dplyr)
 library(devtools)
 
-# Load phenotype data
+# Load phenotype data from a file we saved in the current working directory
 pheno_data = read.csv("UHR_vs_HBR.csv")
 
-# Load ballgown data structure
+# Load ballgown data structure and save it to a variable "bg"
 bg = ballgown(samples=as.vector(pheno_data$path),pData=pheno_data)
+
+# Display a description of this object
+bg
 
 # Load all attributes including gene name
 bg_table = texpr(bg, 'all')
 bg_gene_names = unique(bg_table[, 9:10])
 
-# Save the ballgown object to a file
+# Save the ballgown object to a file for later use
 save(bg, file='bg.rda')
 
 # Perform DE with no filtering
@@ -21,6 +24,7 @@ results_transcripts = stattest(bg, feature="transcript", covariate="type", getFC
 results_genes = stattest(bg, feature="gene", covariate="type", getFC=TRUE, meas="FPKM")
 results_genes = merge(results_genes,bg_gene_names,by.x=c("id"),by.y=c("gene_id"))
 
+# Save a tab delimited file for both the transcript and gene results
 write.table(results_transcripts,"UHR_vs_HBR_transcript_results.tsv",sep="\t")
 write.table(results_genes,"UHR_vs_HBR_gene_results.tsv",sep="\t")
 
