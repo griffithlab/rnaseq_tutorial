b7dba0d5d2e609672678494e926e2a16050ac33a
Malachi Griffith <malachig@gmail.com>
2017-03-26 13:09:06 -0500

    update comparisons script

--- scripts/Tutorial_Module5_Part1_comparisons.R
+++ scripts/Tutorial_Module5_Part1_comparisons.R
@@ -6,6 +6,15 @@
 
 #R tutorial for Informatics for RNA-sequence Analysis workshops
 
+#Load in expression matrix files from each expression method
+working_dir = '/home/ubuntu/workspace/rnaseq/expression'
+setwd(working_dir)
 
+htseq_gene = read.table('htseq_counts/gene_read_counts_table_all_final.tsv', sep="\t", header=TRUE, as.is=1)
 
+stringtie_gene = read.table('stringtie/ref_only/gene_tpm_all_samples.tsv', sep="\t", header=TRUE, as.is=1)
+    
+stringtie/ref_only/transcript_tpm_all_samples.tsv
+kallisto/gene_tpms_all_samples.tsv
+kallisto/transcript_tpms_all_samples.tsv
 
