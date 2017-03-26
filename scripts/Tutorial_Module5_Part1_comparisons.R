a443f514804239876500a44bfe1a9dec926b41c8
Malachi Griffith <malachig@gmail.com>
2017-03-26 12:29:15 -0500

    add R script to compare expression estimates from various method

---
+++ scripts/Tutorial_Module5_Part1_comparisons.R
@@ -0,0 +1,11 @@
+#Tutorial_Module5_Part1_comparisons.R
+
+#Malachi Griffith, mgriffit[AT]wustl.edu
+#Obi Griffith, obigriffith[AT]wustl.edu
+#The McDonnell Genome Institute, Washington University School of Medicine
+
+#R tutorial for Informatics for RNA-sequence Analysis workshops
+
+
+
+
